e0894214a36655586bef541422f658dcafbaf226
nekofied143 <112548898+nekofied143@users.noreply.github.com>
2023-08-05 20:04:20 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,21 +1,389 @@
 // script.js
 const data = [
   {
-    "name": "kaye",
+    "name": "2023-88812633",
     "heads": 2,
     "contribution": -₱6,800,
     "total_loan_balance": ₱7,800,
     "remarks": "IN DEBT OF -6800 PHP"
   },
   {
-    "name": "mary cris",
+    "name": "2023-82495168",
     "heads": 2,
     "contribution": -₱2,450,
     "total_loan_balance": ₱4,450,
     "remarks": "IN DEBT OF -2450 PHP"
   },
   {
-    "name": "lorna",
+    "name": "2023-31247883",
+    "heads": 2,
+    "contribution": ₱2,240,
+    "total_loan_balance": ₱0,
+    "remarks": ""IN-PROGRESS
+AMOUNT TO RECEIVE: ₱2240""
+  },
+  {
+    "name": "2023-26777211",
+    "heads": 2,
+    "contribution": ₱2,240,
+    "total_loan_balance": ₱0,
+    "remarks": ""IN-PROGRESS
+AMOUNT TO RECEIVE: ₱2240""
+  },
+  {
+    "name": "2023-22475295",
+    "heads": 2,
+    "contribution": ₱2,240,
+    "total_loan_balance": ₱0,
+    "remarks": ""IN-PROGRESS
+AMOUNT TO RECEIVE: ₱2240""
+  },
+  {
+    "name": "2023-22475295",
+    "heads": 2,
+    "contribution": ₱2,240,
+    "total_loan_balance": ₱0,
+    "remarks": ""IN-PROGRESS
+AMOUNT TO RECEIVE: ₱2240""
+  },
+  {
+    "name": "2023-22475295",
+    "heads": 2,
+    "contribution": ₱2,240,
+    "total_loan_balance": ₱0,
+    "remarks": ""IN-PROGRESS
+AMOUNT TO RECEIVE: ₱2240""
+  },
+  {
+    "name": "2023-22475295",
+    "heads": 2,
+    "contribution": ₱2,240,
+    "total_loan_balance": ₱0,
+    "remarks": ""IN-PROGRESS
+AMOUNT TO RECEIVE: ₱2240""
+  },
+  {
+    "name": "2023-22475295",
+    "heads": 2,
+    "contribution": ₱2,240,
+    "total_loan_balance": ₱0,
+    "remarks": ""IN-PROGRESS
+AMOUNT TO RECEIVE: ₱2240""
+  },
+  {
+    "name": "2023-22475295",
+    "heads": 2,
+    "contribution": ₱2,240,
+    "total_loan_balance": ₱0,
+    "remarks": ""IN-PROGRESS
+AMOUNT TO RECEIVE: ₱2240""
+  },
+  {
+    "name": "2023-22475295",
+    "heads": 2,
+    "contribution": ₱2,240,
+    "total_loan_balance": ₱0,
+    "remarks": ""IN-PROGRESS
+AMOUNT TO RECEIVE: ₱2240""
+  },
+  {
+    "name": "2023-22475295",
+    "heads": 2,
+    "contribution": ₱2,240,
+    "total_loan_balance": ₱0,
+    "remarks": ""IN-PROGRESS
+AMOUNT TO RECEIVE: ₱2240""
+  },
+  {
+    "name": "2023-22475295",
+    "heads": 2,
+    "contribution": ₱2,240,
+    "total_loan_balance": ₱0,
+    "remarks": ""IN-PROGRESS
+AMOUNT TO RECEIVE: ₱2240""
+  },
+  {
+    "name": "2023-22475295",
+    "heads": 2,
+    "contribution": ₱2,240,
+    "total_loan_balance": ₱0,
+    "remarks": ""IN-PROGRESS
+AMOUNT TO RECEIVE: ₱2240""
+  },
+  {
+    "name": "2023-22475295",
+    "heads": 2,
+    "contribution": ₱2,240,
+    "total_loan_balance": ₱0,
+    "remarks": ""IN-PROGRESS
+AMOUNT TO RECEIVE: ₱2240""
+  },
+  {
+    "name": "2023-22475295",
+    "heads": 2,
+    "contribution": ₱2,240,
+    "total_loan_balance": ₱0,
+    "remarks": ""IN-PROGRESS
+AMOUNT TO RECEIVE: ₱2240""
+  },
+  {
+    "name": "2023-22475295",
+    "heads": 2,
+    "contribution": ₱2,240,
+    "total_loan_balance": ₱0,
+    "remarks": ""IN-PROGRESS
+AMOUNT TO RECEIVE: ₱2240""
+  },
+  {
+    "name": "2023-22475295",
+    "heads": 2,
+    "contribution": ₱2,240,
+    "total_loan_balance": ₱0,
+    "remarks": ""IN-PROGRESS
+AMOUNT TO RECEIVE: ₱2240""
+  },
+  {
+    "name": "2023-22475295",
+    "heads": 2,
+    "contribution": ₱2,240,
+    "total_loan_balance": ₱0,
+    "remarks": ""IN-PROGRESS
+AMOUNT TO RECEIVE: ₱2240""
+  },
+  {
+    "name": "2023-22475295",
+    "heads": 2,
+    "contribution": ₱2,240,
+    "total_loan_balance": ₱0,
+    "remarks": ""IN-PROGRESS
+AMOUNT TO RECEIVE: ₱2240""
+  },
+  {
+    "name": "2023-22475295",
+    "heads": 2,
+    "contribution": ₱2,240,
+    "total_loan_balance": ₱0,
+    "remarks": ""IN-PROGRESS
+AMOUNT TO RECEIVE: ₱2240""
+  },
+  {
+    "name": "2023-22475295",
+    "heads": 2,
+    "contribution": ₱2,240,
+    "total_loan_balance": ₱0,
+    "remarks": ""IN-PROGRESS
+AMOUNT TO RECEIVE: ₱2240""
+  },
+  {
+    "name": "2023-22475295",
+    "heads": 2,
+    "contribution": ₱2,240,
+    "total_loan_balance": ₱0,
+    "remarks": ""IN-PROGRESS
+AMOUNT TO RECEIVE: ₱2240""
+  },
+  {
+    "name": "2023-22475295",
+    "heads": 2,
+    "contribution": ₱2,240,
+    "total_loan_balance": ₱0,
+    "remarks": ""IN-PROGRESS
+AMOUNT TO RECEIVE: ₱2240""
+  },
+  {
+    "name": "2023-22475295",
+    "heads": 2,
+    "contribution": ₱2,240,
+    "total_loan_balance": ₱0,
+    "remarks": ""IN-PROGRESS
+AMOUNT TO RECEIVE: ₱2240""
+  },
+  {
+    "name": "2023-22475295",
+    "heads": 2,
+    "contribution": ₱2,240,
+    "total_loan_balance": ₱0,
+    "remarks": ""IN-PROGRESS
+AMOUNT TO RECEIVE: ₱2240""
+  },
+  {
+    "name": "2023-22475295",
+    "heads": 2,
+    "contribution": ₱2,240,
+    "total_loan_balance": ₱0,
+    "remarks": ""IN-PROGRESS
+AMOUNT TO RECEIVE: ₱2240""
+  },
+  {
+    "name": "2023-22475295",
+    "heads": 2,
+    "contribution": ₱2,240,
+    "total_loan_balance": ₱0,
+    "remarks": ""IN-PROGRESS
+AMOUNT TO RECEIVE: ₱2240""
+  },
+  {
+    "name": "2023-22475295",
+    "heads": 2,
+    "contribution": ₱2,240,
+    "total_loan_balance": ₱0,
+    "remarks": ""IN-PROGRESS
+AMOUNT TO RECEIVE: ₱2240""
+  },
+  {
+    "name": "2023-22475295",
+    "heads": 2,
+    "contribution": ₱2,240,
+    "total_loan_balance": ₱0,
+    "remarks": ""IN-PROGRESS
+AMOUNT TO RECEIVE: ₱2240""
+  },
+  {
+    "name": "2023-22475295",
+    "heads": 2,
+    "contribution": ₱2,240,
+    "total_loan_balance": ₱0,
+    "remarks": ""IN-PROGRESS
+AMOUNT TO RECEIVE: ₱2240""
+  },
+  {
+    "name": "2023-22475295",
+    "heads": 2,
+    "contribution": ₱2,240,
+    "total_loan_balance": ₱0,
+    "remarks": ""IN-PROGRESS
+AMOUNT TO RECEIVE: ₱2240""
+  },
+  {
+    "name": "2023-22475295",
+    "heads": 2,
+    "contribution": ₱2,240,
+    "total_loan_balance": ₱0,
+    "remarks": ""IN-PROGRESS
+AMOUNT TO RECEIVE: ₱2240""
+  },
+  {
+    "name": "2023-22475295",
+    "heads": 2,
+    "contribution": ₱2,240,
+    "total_loan_balance": ₱0,
+    "remarks": ""IN-PROGRESS
+AMOUNT TO RECEIVE: ₱2240""
+  },
+  {
+    "name": "2023-22475295",
+    "heads": 2,
+    "contribution": ₱2,240,
+    "total_loan_balance": ₱0,
+    "remarks": ""IN-PROGRESS
+AMOUNT TO RECEIVE: ₱2240""
+  },
+  {
+    "name": "2023-22475295",
+    "heads": 2,
+    "contribution": ₱2,240,
+    "total_loan_balance": ₱0,
+    "remarks": ""IN-PROGRESS
+AMOUNT TO RECEIVE: ₱2240""
+  },
+  {
+    "name": "2023-22475295",
+    "heads": 2,
+    "contribution": ₱2,240,
+    "total_loan_balance": ₱0,
+    "remarks": ""IN-PROGRESS
+AMOUNT TO RECEIVE: ₱2240""
+  },
+  {
+    "name": "2023-22475295",
+    "heads": 2,
+    "contribution": ₱2,240,
+    "total_loan_balance": ₱0,
+    "remarks": ""IN-PROGRESS
+AMOUNT TO RECEIVE: ₱2240""
+  },
+  {
+    "name": "2023-22475295",
+    "heads": 2,
+    "contribution": ₱2,240,
+    "total_loan_balance": ₱0,
+    "remarks": ""IN-PROGRESS
+AMOUNT TO RECEIVE: ₱2240""
+  },
+  {
+    "name": "2023-22475295",
+    "heads": 2,
+    "contribution": ₱2,240,
+    "total_loan_balance": ₱0,
+    "remarks": ""IN-PROGRESS
+AMOUNT TO RECEIVE: ₱2240""
+  },
+  {
+    "name": "2023-22475295",
+    "heads": 2,
+    "contribution": ₱2,240,
+    "total_loan_balance": ₱0,
+    "remarks": ""IN-PROGRESS
+AMOUNT TO RECEIVE: ₱2240""
+  },
+  {
+    "name": "2023-22475295",
+    "heads": 2,
+    "contribution": ₱2,240,
+    "total_loan_balance": ₱0,
+    "remarks": ""IN-PROGRESS
+AMOUNT TO RECEIVE: ₱2240""
+  },
+  {
+    "name": "2023-22475295",
+    "heads": 2,
+    "contribution": ₱2,240,
+    "total_loan_balance": ₱0,
+    "remarks": ""IN-PROGRESS
+AMOUNT TO RECEIVE: ₱2240""
+  },
+  {
+    "name": "2023-22475295",
+    "heads": 2,
+    "contribution": ₱2,240,
+    "total_loan_balance": ₱0,
+    "remarks": ""IN-PROGRESS
+AMOUNT TO RECEIVE: ₱2240""
+  },
+  {
+    "name": "2023-22475295",
+    "heads": 2,
+    "contribution": ₱2,240,
+    "total_loan_balance": ₱0,
+    "remarks": ""IN-PROGRESS
+AMOUNT TO RECEIVE: ₱2240""
+  },
+  {
+    "name": "2023-22475295",
+    "heads": 2,
+    "contribution": ₱2,240,
+    "total_loan_balance": ₱0,
+    "remarks": ""IN-PROGRESS
+AMOUNT TO RECEIVE: ₱2240""
+  },
+  {
+    "name": "2023-22475295",
+    "heads": 2,
+    "contribution": ₱2,240,
+    "total_loan_balance": ₱0,
+    "remarks": ""IN-PROGRESS
+AMOUNT TO RECEIVE: ₱2240""
+  },
+  {
+    "name": "2023-22475295",
+    "heads": 2,
+    "contribution": ₱2,240,
+    "total_loan_balance": ₱0,
+    "remarks": ""IN-PROGRESS
+AMOUNT TO RECEIVE: ₱2240""
+  },
+  {
+    "name": "2023-22475295",
     "heads": 2,
     "contribution": ₱2,240,
     "total_loan_balance": ₱0,
