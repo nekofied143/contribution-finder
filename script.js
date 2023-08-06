61bb237c7cb65f7dd62c365177be0e1ab1426e0b
nekofied143 <112548898+nekofied143@users.noreply.github.com>
2023-08-06 21:27:52 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -1,1 +1,360 @@
-const data=[{name:"2023-88812633",heads:2,contribution:1e3,total_loan_balance:7800,remarks:"Current month: February. In debt of 6,800."},{name:"2023-82495168",heads:2,contribution:2e3,total_loan_balance:4450,remarks:"Current month: April. In debt of 2,450."},{name:"2023-31247883",heads:2,contribution:2240,total_loan_balance:0,remarks:"Current month: April."},{name:"2023-26777211",heads:2,contribution:3100,total_loan_balance:0,remarks:"Current month: June."},{name:"2023-22475295",heads:2,contribution:3800,total_loan_balance:0,remarks:"Current month: July."},{name:"2023-16849664",heads:3,contribution:5430,total_loan_balance:0,remarks:"Current month: June."},{name:"2023-69133791",heads:2,contribution:4240,total_loan_balance:0,remarks:"Current month: July."},{name:"2023-20390853",heads:2,contribution:4240,total_loan_balance:0,remarks:"Current month: July."},{name:"2023-14419159",heads:2,contribution:1990,total_loan_balance:7600,remarks:"Current month: April. In debt of 5,610"},{name:"2023-13716823",heads:3,contribution:5e3,total_loan_balance:0,remarks:"Current month: June."},{name:"2023-72045449",heads:1,contribution:590,total_loan_balance:4500,remarks:"Current month: February. In debt of 3,910"},{name:"2023-10231996",heads:2,contribution:4240,total_loan_balance:0,remarks:"Current month: July."},{name:"2023-09021996",heads:5,contribution:18250,total_loan_balance:0,remarks:"Completed the contribution."},{name:"2023-82742042",heads:2,contribution:4270,total_loan_balance:0,remarks:"Current month: August."},{name:"2023-42380641",heads:2,contribution:650,total_loan_balance:0,remarks:"Current month: February."},{name:"2023-27373139",heads:2,contribution:3020,total_loan_balance:0,remarks:"Current month: May."},{name:"2023-78377119",heads:2,contribution:3620,total_loan_balance:0,remarks:"Current month: June."},{name:"2023-15914771",heads:5,contribution:10600,total_loan_balance:0,remarks:"Current month: July."},{name:"2023-46754777",heads:1,contribution:590,total_loan_balance:4700,remarks:"Current month: February. In debt of 4,110"},{name:"2023-44664972",heads:2,contribution:620,total_loan_balance:0,remarks:"Current month: January."},{name:"2023-22746266",heads:5,contribution:10150,total_loan_balance:0,remarks:"Current month: July."},{name:"2023-84772048",heads:5,contribution:9050,total_loan_balance:0,remarks:"Current month: June."},{name:"2023-65004749",heads:2,contribution:3020,total_loan_balance:4200,remarks:"Current month: May. In debt of 1,180"},{name:"2023-52162295",heads:2,contribution:1400,total_loan_balance:0,remarks:"Current month: March."},{name:"2023-94301759",heads:2,contribution:3320,total_loan_balance:1200,remarks:"Current month: June."},{name:"2023-44420688",heads:2,contribution:3620,total_loan_balance:0,remarks:"Current month: June."},{name:"2023-81589758",heads:5,contribution:4550,total_loan_balance:0,remarks:"Current month: April."},{name:"2023-19148758",heads:5,contribution:4550,total_loan_balance:0,remarks:"Current month: April."},{name:"2023-73780810",heads:5,contribution:10650,total_loan_balance:0,remarks:"Current month: August."},{name:"2023-45828884",heads:1,contribution:2110,total_loan_balance:0,remarks:"Current month: July."},{name:"2023-10694296",heads:1,contribution:2110,total_loan_balance:0,remarks:"Current month: July."},{name:"2023-34140960",heads:1,contribution:2110,total_loan_balance:0,remarks:"Current month: July."},{name:"2023-47529130",heads:2,contribution:4640,total_loan_balance:0,remarks:"Current month: August."},{name:"2023-66323649",heads:2,contribution:3260,total_loan_balance:0,remarks:"Current month: June."},{name:"2023-28033548",heads:3,contribution:3600,total_loan_balance:1300,remarks:"Current month: April."},{name:"2023-46555468",heads:2,contribution:2260,total_loan_balance:8200,remarks:"Current month: April. In debt of 5,940."},{name:"2023-29124040",heads:2,contribution:2540,total_loan_balance:8400,remarks:"Current month: May. In debt of 5,860."},{name:"2023-37240217",heads:4,contribution:4800,total_loan_balance:3900,remarks:"Current month: April."},{name:"2023-17274476",heads:4,contribution:4520,total_loan_balance:2800,remarks:"Current month: April."},{name:"2023-86749518",heads:1,contribution:1200,total_loan_balance:0,remarks:"Current month: April."},{name:"2023-72667050",heads:1,contribution:1270,total_loan_balance:0,remarks:"Current month: May."},{name:"2023-62274550",heads:2,contribution:2120,total_loan_balance:8400,remarks:"Current month: April. In debt of 6,280."},{name:"2023-50458125",heads:2,contribution:4240,total_loan_balance:2e3,remarks:"Current month: July."},{name:"2023-58087768",heads:1,contribution:1810,total_loan_balance:1200,remarks:"Current month: June."},{name:"2023-68227765",heads:2,contribution:3920,total_loan_balance:0,remarks:"Current month: July."},{name:"2023-29326660",heads:3,contribution:6400,total_loan_balance:3300,remarks:"Current month: August."},{name:"2023-41482912",heads:1,contribution:310,total_loan_balance:4500,remarks:"Current month: January. In debt of 4,190"},{name:"2023-89595150",heads:1,contribution:0,total_loan_balance:0,remarks:"Current month: None."}],searchInput=document.getElementById("searchInput"),legendsContainer=document.querySelector(".legends-container"),calculatorContainer=document.querySelector(".calculator-container"),loandebtContainer=document.querySelector(".loan-debt-container"),headsData=document.getElementById("headsData"),contributionData=document.getElementById("contributionData"),loanBalanceData=document.getElementById("loanBalanceData"),remarksData=document.getElementById("remarksData"),dataTable=document.getElementById("dataTable"),currentDateElement=document.getElementById("currentDate"),contributionCalculationElement=document.getElementById("contributionCalculation"),loanDebtCalculationElement=document.getElementById("loanDebtCalculation"),estimatedDateElement=document.getElementById("estimatedDate"),estimatedDate1Element=document.getElementById("estimatedDate1");function toggleLegends(){const t=document.querySelector(".legends-text"),n=document.querySelector(".legends-content");n.style.display="block"===n.style.display?"none":"block",t.textContent="block"===n.style.display?"Click me ⋎":"Click me ≻"}function toggleCalculator(){const t=document.querySelector(".calculator-text"),n=document.querySelector(".calculator-content");n.style.display="block"===n.style.display?"none":"block",t.textContent="block"===n.style.display?"CONTRIBUTION DATE CALCULATOR ⋎":"CONTRIBUTION DATE CALCULATOR ≻"}function toggleLoanDebt(){const t=document.querySelector(".loan-debt-text"),n=document.querySelector(".loan-debt-content");n.style.display="block"===n.style.display?"none":"block",t.textContent="block"===n.style.display?"LOAN/DEBT CALCULATOR ⋎":"LOAN/DEBT CALCULATOR ≻"}function search(){const t=searchInput.value.trim().toLowerCase();if(!t)return void clearData();const n=data.find(n=>n.name.toLowerCase()===t);n?displayData(n):displayError("No data exist!")}function displayData(t){headsData.textContent=t.heads,contributionData.textContent=t.contribution,loanBalanceData.textContent=t.total_loan_balance,remarksData.textContent=t.remarks;const n=t.contribution,e=t.heads,a=t.total_loan_balance,o=new Date;currentDateElement.textContent=o.toLocaleDateString();const r=n/(10*e);contributionCalculationElement.textContent=`₱${n} / ₱10 / ${e} = ${r} days`;const l=new Date(o.getFullYear(),0,1+r-1);estimatedDateElement.textContent=l.toLocaleDateString();const c=new Date(o.getFullYear(),0,1+r-1);estimatedDate1Element.textContent=c.toLocaleDateString();const m=n-a;loanDebtCalculationElement.textContent=`₱${n} - ₱${a} = ₱${m}`}function clearData(){headsData.textContent="",contributionData.textContent="",loanBalanceData.textContent="",remarksData.textContent=""}function displayError(t){headsData.textContent=t,contributionData.textContent=t,loanBalanceData.textContent=t,remarksData.textContent=t}legendsContainer.addEventListener("click",()=>{toggleLegends()}),calculatorContainer.addEventListener("click",()=>{toggleCalculator()}),loandebtContainer.addEventListener("click",()=>{toggleLoanDebt()}),searchInput.addEventListener("keyup",search);
+const data = [{
+    name: "2023-88812633",
+    heads: 2,
+    contribution: 1e3,
+    total_loan_balance: 7800,
+    remarks: "Current month: February. In debt of 6,800."
+ }, {
+    name: "2023-82495168",
+    heads: 2,
+    contribution: 2e3,
+    total_loan_balance: 4450,
+    remarks: "Current month: April. In debt of 2,450."
+ }, {
+    name: "2023-31247883",
+    heads: 2,
+    contribution: 2240,
+    total_loan_balance: 0,
+    remarks: "Current month: April."
+ }, {
+    name: "2023-26777211",
+    heads: 2,
+    contribution: 3100,
+    total_loan_balance: 0,
+    remarks: "Current month: June."
+ }, {
+    name: "2023-22475295",
+    heads: 2,
+    contribution: 3800,
+    total_loan_balance: 0,
+    remarks: "Current month: July."
+ }, {
+    name: "2023-16849664",
+    heads: 3,
+    contribution: 6360,
+    total_loan_balance: 0,
+    remarks: "Current month: June."
+ }, {
+    name: "2023-69133791",
+    heads: 2,
+    contribution: 4240,
+    total_loan_balance: 0,
+    remarks: "Current month: July."
+ }, {
+    name: "2023-20390853",
+    heads: 2,
+    contribution: 4240,
+    total_loan_balance: 0,
+    remarks: "Current month: July."
+ }, {
+    name: "2023-14419159",
+    heads: 2,
+    contribution: 4860,
+    total_loan_balance: 7600,
+    remarks: "Current month: August. In debt of 2740"
+ }, {
+    name: "2023-13716823",
+    heads: 3,
+    contribution: 5e3,
+    total_loan_balance: 0,
+    remarks: "Current month: June."
+ }, {
+    name: "2023-72045449",
+    heads: 1,
+    contribution: 590,
+    total_loan_balance: 4500,
+    remarks: "Current month: February. In debt of 3,910"
+ }, {
+    name: "2023-10231996",
+    heads: 2,
+    contribution: 4240,
+    total_loan_balance: 0,
+    remarks: "Current month: July."
+ }, {
+    name: "2023-09021996",
+    heads: 5,
+    contribution: 18250,
+    total_loan_balance: 0,
+    remarks: "Completed the contribution."
+ }, {
+    name: "2023-82742042",
+    heads: 2,
+    contribution: 4270,
+    total_loan_balance: 0,
+    remarks: "Current month: August."
+ }, {
+    name: "2023-42380641",
+    heads: 2,
+    contribution: 650,
+    total_loan_balance: 0,
+    remarks: "Current month: February."
+ }, {
+    name: "2023-27373139",
+    heads: 2,
+    contribution: 3020,
+    total_loan_balance: 0,
+    remarks: "Current month: May."
+ }, {
+    name: "2023-78377119",
+    heads: 2,
+    contribution: 3620,
+    total_loan_balance: 0,
+    remarks: "Current month: June."
+ }, {
+    name: "2023-15914771",
+    heads: 5,
+    contribution: 10600,
+    total_loan_balance: 0,
+    remarks: "Current month: July."
+ }, {
+    name: "2023-46754777",
+    heads: 1,
+    contribution: 590,
+    total_loan_balance: 4700,
+    remarks: "Current month: February. In debt of 4,110"
+ }, {
+    name: "2023-44664972",
+    heads: 2,
+    contribution: 620,
+    total_loan_balance: 0,
+    remarks: "Current month: January."
+ }, {
+    name: "2023-22746266",
+    heads: 5,
+    contribution: 10150,
+    total_loan_balance: 0,
+    remarks: "Current month: July."
+ }, {
+    name: "2023-84772048",
+    heads: 5,
+    contribution: 9050,
+    total_loan_balance: 0,
+    remarks: "Current month: June."
+ }, {
+    name: "2023-65004749",
+    heads: 2,
+    contribution: 3020,
+    total_loan_balance: 3650,
+    remarks: "Current month: May. In debt of 630"
+ }, {
+    name: "2023-52162295",
+    heads: 2,
+    contribution: 1400,
+    total_loan_balance: 0,
+    remarks: "Current month: March."
+ }, {
+    name: "2023-94301759",
+    heads: 2,
+    contribution: 3320,
+    total_loan_balance: 1200,
+    remarks: "Current month: June."
+ }, {
+    name: "2023-44420688",
+    heads: 2,
+    contribution: 3620,
+    total_loan_balance: 0,
+    remarks: "Current month: June."
+ }, {
+    name: "2023-81589758",
+    heads: 5,
+    contribution: 4550,
+    total_loan_balance: 0,
+    remarks: "Current month: April."
+ }, {
+    name: "2023-19148758",
+    heads: 5,
+    contribution: 4550,
+    total_loan_balance: 0,
+    remarks: "Current month: April."
+ }, {
+    name: "2023-73780810",
+    heads: 5,
+    contribution: 10650,
+    total_loan_balance: 0,
+    remarks: "Current month: August."
+ }, {
+    name: "2023-45828884",
+    heads: 1,
+    contribution: 2110,
+    total_loan_balance: 0,
+    remarks: "Current month: July."
+ }, {
+    name: "2023-10694296",
+    heads: 1,
+    contribution: 2110,
+    total_loan_balance: 0,
+    remarks: "Current month: July."
+ }, {
+    name: "2023-34140960",
+    heads: 1,
+    contribution: 2110,
+    total_loan_balance: 0,
+    remarks: "Current month: July."
+ }, {
+    name: "2023-47529130",
+    heads: 2,
+    contribution: 4640,
+    total_loan_balance: 0,
+    remarks: "Current month: August."
+ }, {
+    name: "2023-66323649",
+    heads: 2,
+    contribution: 3860,
+    total_loan_balance: 0,
+    remarks: "Current month: June."
+ }, {
+    name: "2023-28033548",
+    heads: 3,
+    contribution: 4440,
+    total_loan_balance: 100,
+    remarks: "Current month: May."
+ }, {
+    name: "2023-46555468",
+    heads: 2,
+    contribution: 2540,
+    total_loan_balance: 8200,
+    remarks: "Current month: May. In debt of 5,660."
+ }, {
+    name: "2023-29124040",
+    heads: 2,
+    contribution: 3100,
+    total_loan_balance: 8400,
+    remarks: "Current month: June. In debt of 5,300."
+ }, {
+    name: "2023-37240217",
+    heads: 4,
+    contribution: 5780,
+    total_loan_balance: 3900,
+    remarks: "Current month: May."
+ }, {
+    name: "2023-17274476",
+    heads: 4,
+    contribution: 5080,
+    total_loan_balance: 200,
+    remarks: "Current month: May."
+ }, {
+    name: "2023-86749518",
+    heads: 1,
+    contribution: 1340,
+    total_loan_balance: 0,
+    remarks: "Current month: May."
+ }, {
+    name: "2023-72667050",
+    heads: 1,
+    contribution: 1690,
+    total_loan_balance: 0,
+    remarks: "Current month: June."
+ }, {
+    name: "2023-62274550",
+    heads: 2,
+    contribution: 2120,
+    total_loan_balance: 8400,
+    remarks: "Current month: April. In debt of 6,280."
+ }, {
+    name: "2023-50458125",
+    heads: 2,
+    contribution: 4240,
+    total_loan_balance: 2e3,
+    remarks: "Current month: July."
+ }, {
+    name: "2023-58087768",
+    heads: 1,
+    contribution: 2120,
+    total_loan_balance: 1200,
+    remarks: "Current month: July."
+ }, {
+    name: "2023-68227765",
+    heads: 2,
+    contribution: 4220,
+    total_loan_balance: 0,
+    remarks: "Current month: July."
+ }, {
+    name: "2023-29326660",
+    heads: 3,
+    contribution: 6400,
+    total_loan_balance: 3300,
+    remarks: "Current month: August."
+ }, {
+    name: "2023-41482912",
+    heads: 1,
+    contribution: 310,
+    total_loan_balance: 4500,
+    remarks: "Current month: January. In debt of 4,190"
+ }, {
+    name: "2023-89595150",
+    heads: 1,
+    contribution: 0,
+    total_loan_balance: 0,
+    remarks: "Current month: None."
+ }],
+ searchInput = document.getElementById("searchInput"),
+ legendsContainer = document.querySelector(".legends-container"),
+ calculatorContainer = document.querySelector(".calculator-container"),
+ loandebtContainer = document.querySelector(".loan-debt-container"),
+ headsData = document.getElementById("headsData"),
+ contributionData = document.getElementById("contributionData"),
+ loanBalanceData = document.getElementById("loanBalanceData"),
+ remarksData = document.getElementById("remarksData"),
+ dataTable = document.getElementById("dataTable"),
+ currentDateElement = document.getElementById("currentDate"),
+ contributionCalculationElement = document.getElementById("contributionCalculation"),
+ loanDebtCalculationElement = document.getElementById("loanDebtCalculation"),
+ estimatedDateElement = document.getElementById("estimatedDate"),
+ estimatedDate1Element = document.getElementById("estimatedDate1");
+
+function toggleLegends() {
+ const t = document.querySelector(".legends-text"),
+    n = document.querySelector(".legends-content");
+ n.style.display = "block" === n.style.display ? "none" : "block", t.textContent = "block" === n.style.display ? "Click me ⋎" : "Click me ≻"
+}
+
+function toggleCalculator() {
+ const t = document.querySelector(".calculator-text"),
+    n = document.querySelector(".calculator-content");
+ n.style.display = "block" === n.style.display ? "none" : "block", t.textContent = "block" === n.style.display ? "CONTRIBUTION DATE CALCULATOR ⋎" : "CONTRIBUTION DATE CALCULATOR ≻"
+}
+
+function toggleLoanDebt() {
+ const t = document.querySelector(".loan-debt-text"),
+    n = document.querySelector(".loan-debt-content");
+ n.style.display = "block" === n.style.display ? "none" : "block", t.textContent = "block" === n.style.display ? "LOAN/DEBT CALCULATOR ⋎" : "LOAN/DEBT CALCULATOR ≻"
+}
+
+function search() {
+ const t = searchInput.value.trim().toLowerCase();
+ if (!t) return void clearData();
+ const n = data.find(n => n.name.toLowerCase() === t);
+ n ? displayData(n) : displayError("No data exist!")
+}
+
+function displayData(t) {
+ headsData.textContent = t.heads, contributionData.textContent = t.contribution, loanBalanceData.textContent = t.total_loan_balance, remarksData.textContent = t.remarks;
+ const n = t.contribution,
+    e = t.heads,
+    a = t.total_loan_balance,
+    o = new Date;
+ currentDateElement.textContent = o.toLocaleDateString();
+ const r = n / (10 * e);
+ contributionCalculationElement.textContent = `₱${n} / ₱10 / ${e} = ${r} days`;
+ const l = new Date(o.getFullYear(), 0, 1 + r - 1);
+ estimatedDateElement.textContent = l.toLocaleDateString();
+ const c = new Date(o.getFullYear(), 0, 1 + r - 1);
+ estimatedDate1Element.textContent = c.toLocaleDateString();
+ const m = n - a;
+ loanDebtCalculationElement.textContent = `₱${n} - ₱${a} = ₱${m}`
+}
+
+function clearData() {
+ headsData.textContent = "", contributionData.textContent = "", loanBalanceData.textContent = "", remarksData.textContent = ""
+}
+
+function displayError(t) {
+ headsData.textContent = t, contributionData.textContent = t, loanBalanceData.textContent = t, remarksData.textContent = t
+}
+legendsContainer.addEventListener("click", () => {
+ toggleLegends()
+}), calculatorContainer.addEventListener("click", () => {
+ toggleCalculator()
+}), loandebtContainer.addEventListener("click", () => {
+ toggleLoanDebt()
+}), searchInput.addEventListener("keyup", search);
